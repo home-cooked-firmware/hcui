dde38adb254eb6c53e203b178b6b7f6538504468
Joe D'Amore <joe@joedamore.me>
2025-06-18 19:29:36 -0400

    WIP

--- build.rs
+++ build.rs
@@ -1,3 +1,12 @@
 fn main() {
-    slint_build::compile("ui/app-window.slint").expect("Slint build failed");
+    let manifest_dir = std::path::PathBuf::from(std::env::var_os("CARGO_MANIFEST_DIR").unwrap());
+
+    let include_paths = vec![
+        manifest_dir.join("ui"),
+    ];
+
+    let config = slint_build::CompilerConfiguration::new()
+        .with_include_paths(include_paths);
+
+    slint_build::compile_with_config("ui/app-window.slint", config).expect("Slint build failed");
 }
